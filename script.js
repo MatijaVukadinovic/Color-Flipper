cf2b857648877fcbd6359cd17e0fbc1cb61d824f
MatijaVukadinovic <152809747+MatijaVukadinovic@users.noreply.github.com>
2024-04-16 18:31:16 +0200

    Declare function for random number generation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Array of hexadecimal values
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
-//Declaration of
+//Declaration of color and button reference
 let color = document.querySelector(".color")
 let button = document.getElementById("btn");
+
+//Declaration of random number generator function
+function generateRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
