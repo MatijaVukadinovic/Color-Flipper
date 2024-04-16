4e71f43e7076f2ea6c277757addd290da558b87f
MatijaVukadinovic <152809747+MatijaVukadinovic@users.noreply.github.com>
2024-04-16 18:21:30 +0200

    Declare color and button reference variables

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //Array of hexadecimal values
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+
+//Declaration of
+let color = document.querySelector(".color")
+let button = document.getElementById("btn");
